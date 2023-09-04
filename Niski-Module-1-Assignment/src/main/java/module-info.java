d388952b58c9313ec1559b20d15bc3eda12ff2ef
Brian Niski <niskibrian@hotmail.com>
2023-09-04 09:32:20 -0400

    Starter Project
    Create JavaFX Project in IntelliJ

---
+++ Niski-Module-1-Assignment/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module niski.farmingdale.niskimodule1assignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens niski.farmingdale.niskimodule1assignment to javafx.fxml;
+    exports niski.farmingdale.niskimodule1assignment;
+}
